1f3e83ca600483ec9d1089c225d984cd800a0218
Nicolas T <nicolas.turlais@gmail.com>
2014-05-07 11:12:48 +0200

    removed dependencies, helping bower integration

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require "lib/lessphp/lessc.inc.php";
 
 $less = new lessc;
-$less->compileFile("css/style.less", "css/style.css");
+$less->compileFile("css/mosaic-css.less", "css/mosaic.css");
 
 include('index.html');
 ?>
